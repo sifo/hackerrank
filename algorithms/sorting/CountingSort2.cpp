553d436b786f118e11407ecb95b1e20594a937ea
Jean Vilver <jean.vilver@gmail.com>
2018-01-05 21:36:49 +0100

    Add Counting Sort 2

---
+++ algorithms/sorting/CountingSort2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n, cur;
+    cin >> n;
+    int t[100] = { 0 };
+    for(int i = 0; i < n; i++) {
+        cin >> cur;
+        if(cur < 100)
+            t[cur]++;
+    }
+    for(int i = 0; i < 100; i++) {
+        for(int j = 0; j < t[i]; j++) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
