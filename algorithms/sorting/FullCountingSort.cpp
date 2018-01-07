b792f91ade322d942d587678d9692afd8cd80c82
Jean Vilver <jean.vilver@gmail.com>
2018-01-07 14:09:18 +0100

    Add Full Counting Sort

---
+++ algorithms/sorting/FullCountingSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <tuple>
+using namespace std;
+
+int main() {
+    int n, cur;
+    string s;
+    cin >> n;
+    vector<string> t[100];
+    for(int i = 0; i < n; i++) {
+        cin >> cur;
+        cin >> s;
+        if(i < n/2) {
+            s = "-";
+        }
+        if(cur < 100) {
+            t[cur].push_back(s);
+        }
+    }
+    for(int i = 0; i < 100; i++) {
+        for(unsigned j = 0; j < t[i].size(); j++) {
+            cout << t[i].at(j) << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
