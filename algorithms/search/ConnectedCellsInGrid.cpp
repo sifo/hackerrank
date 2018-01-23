4b9251014446a49528ab62ea502b877398116047
Jean Vilver <jean.vilver@gmail.com>
2018-01-23 01:34:10 +0100

    Refactor

--- algorithms/search/ConnectedCellsInGrid.cpp
+++ algorithms/search/ConnectedCellsInGrid.cpp
@@ -17,9 +17,9 @@ bool in_matrix(int n, int m, Position p) {
     return p.x < n && p.y < m && p.x >= 0 && p.y >= 0;
 }
 
-int region_size(int **t, bool **visited, int i, int j, int n, int m) {
+int region_size(int t[], bool visited[], int i, int j, int n, int m) {
     Position c = {i, j};
-    visited[c.x][c.y] = true;
+    visited[c.x*m+c.y] = true;
     list<Position> queue;
     queue.push_back(c);
     int res = 1;
@@ -37,8 +37,8 @@ int region_size(int **t, bool **visited, int i, int j, int n, int m) {
         adj.push_back({ c.x-1, c.y   });
         adj.push_back({ c.x,   c.y-1 });
         for(auto p: adj) {
-            if(in_matrix(n, m, p) && t[p.x][p.y] == 1 && !visited[p.x][p.y]) {
-                visited[p.x][p.y] = true;
+            if(in_matrix(n, m, p) && t[p.x*m+p.y] == 1 && !visited[p.x*m+p.y]) {
+                visited[p.x*m+p.y] = true;
                 queue.push_back(p);
                 res++;
             }
@@ -51,21 +51,17 @@ int region_size(int **t, bool **visited, int i, int j, int n, int m) {
 int main() {
     int n, m;
     cin >> n >> m;
-    int **t = new int *[n];
-    bool **visited = new bool *[n];
-    for(int i = 0; i < n; i++) {
-        t[i] = new int[m];
-        visited[i] = new bool[m];
-    }
+    int t[n*m] = { 0 };
+    bool visited[n*m] = { false };
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            cin >> t[i][j];
+            cin >> t[i*m+j];
         }
     }
     int res = 0;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(t[i][j] == 1 && !visited[i][j]) {
+            if(t[i*m+j] == 1 && !visited[i*m+j]) {
                 int tmp = region_size(t, visited, i, j, n, m);
                 if(tmp > res)
                     res = tmp;
