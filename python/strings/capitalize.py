5ed7678c2d4ea8bc2bfb094cf9028f9a93fb16da
Jean Vilver <jean.vilver@gmail.com>
2018-01-25 06:17:10 +0100

    Add Capitalize

---
+++ python/strings/capitalize.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/capitalize/problem
+
+def capitalize(string):
+    return ' '.join([n.capitalize() for n in string.split(' ')])
+
+if __name__ == '__main__':
+    string = input()
+    capitalized_string = capitalize(string)
+    print(capitalized_string)
