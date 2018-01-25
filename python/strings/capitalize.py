01740205f74f826cd0c9330c4efc3f8f705b31fa
Jean Vilver <jean.vilver@gmail.com>
2018-01-25 06:20:29 +0100

    Add alternative solution to Capitalize

--- python/strings/capitalize.py
+++ python/strings/capitalize.py
@@ -3,6 +3,19 @@
 def capitalize(string):
     return ' '.join([n.capitalize() for n in string.split(' ')])
 
+def capitalize2(string):
+    flag = True
+    res = ""
+    for l in string:
+        if l != " " and flag:
+            if l.isalpha():
+                l = l.upper()
+            flag = False
+        elif l == " ":
+            flag = True
+        res += l
+    return res
+
 if __name__ == '__main__':
     string = input()
     capitalized_string = capitalize(string)
