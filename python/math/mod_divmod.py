72b3bafae9df40e3030133a2129b1561d0d27bf2
Jean Vilver <jean.vilver@gmail.com>
2018-01-29 15:17:36 +0100

    Add Mod Divmod

---
+++ python/math/mod_divmod.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+if __name__ == '__main__':
+    r = divmod(int(input()), int(input()))
+    print(r[0])
+    print(r[1])
+    print(r)
