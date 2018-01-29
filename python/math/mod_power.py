4c37c049c9dcf2b83bc880bfa73ba6698070018f
Jean Vilver <jean.vilver@gmail.com>
2018-01-29 15:22:01 +0100

    Add Power - Mod Power

---
+++ python/math/mod_power.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    print(a**b)
+    print(pow(a, b, m))
