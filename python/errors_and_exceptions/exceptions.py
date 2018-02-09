f3daf5b54e5588947f113f2aeb4dfaf0c5e3095a
Jean Vilver <jean.vilver@gmail.com>
2018-02-09 15:29:05 +0100

    Add Exceptions

---
+++ python/errors_and_exceptions/exceptions.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/exceptions/problem
+
+if __name__ == '__main__':
+    for i in range(int(input())):
+        try:
+            a, b = map(int, input().split())
+            print(a//b)
+        except Exception as e:
+            print("Error Code:", e)
