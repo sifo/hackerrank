d4168554e8c198cb3a2d18a35f934612468d4a43
Jean Vilver <jean.vilver@gmail.com>
2018-02-22 22:58:29 +0100

    Add Matrix Script

---
+++ python/regex_and_parsing/matrix_script.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/matrix-script/problem
+
+import re
+
+n, _ = input().split()
+l = [list(input()) for _ in range(int(n))]
+l = [''.join(i) for i in zip(*l)]
+l = ''.join(l)
+p = r'(?<=[a-zA-Z0-9])[\s\!\@\#\$\%\&]+(?=[a-zA-Z0-9])'
+print(re.sub(p, ' ', l))
