d29bd05ff3ad422a8b58354b36eb5a7b53c817fe
Jean Vilver <jean.vilver@gmail.com>
2018-02-22 22:59:56 +0100

    Refactor

--- python/regex_and_parsing/matrix_script.py
+++ python/regex_and_parsing/matrix_script.py
@@ -6,5 +6,6 @@ n, _ = input().split()
 l = [list(input()) for _ in range(int(n))]
 l = [''.join(i) for i in zip(*l)]
 l = ''.join(l)
-p = r'(?<=[a-zA-Z0-9])[\s\!\@\#\$\%\&]+(?=[a-zA-Z0-9])'
+# p = r'(?<=[a-zA-Z0-9])[\s\!\@\#\$\%\&]+(?=[a-zA-Z0-9])'
+p = r'(?<=\w)[^\w]+(?=\w)'
 print(re.sub(p, ' ', l))
