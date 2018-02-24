21f07b60f2410809347bfece21b9536472ceac4d
Jean Vilver <jean.vilver@gmail.com>
2018-02-24 23:10:00 +0100

    Add Standardize Mobile Number Using Decorators

---
+++ python/closures_and_decorators/mobile_number.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/standardize-mobile-number-using-decorators/problem
+
+def wrapper(f):
+    def fun(l):
+        f(['+91 {} {}'.format(p[-10:-5], p[-5:]) for p in l])
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l)
