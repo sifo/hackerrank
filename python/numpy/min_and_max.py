48b2b40b9438cde1e2801ff14ff22fa24acc6de5
Jean Vilver <jean.vilver@gmail.com>
2018-03-02 20:34:38 +0100

    Refactor

--- python/numpy/min_and_max.py
+++ python/numpy/min_and_max.py
@@ -4,7 +4,7 @@ import numpy
 
 if __name__ == '__main__':
     n, _ = input().split()
-    l = [ list(map(int, input().split())) for _ in range(int(n))]
+    l = [list(map(int, input().split())) for _ in range(int(n))]
     a = numpy.array(l)
     s = numpy.min(a, axis=1)
     print(numpy.max(s))
