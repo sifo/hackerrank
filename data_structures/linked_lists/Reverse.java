a57460b8e0bc57b438b7a5c099bf7b186b3fe0a7
Jean Vilver <jean.vilver@gmail.com>
2018-03-07 23:09:16 +0100

    Add Reverse a linked list

---
+++ data_structures/linked_lists/Reverse.java
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/reverse-a-linked-list/problem
+
+Node Reverse(Node head) {
+    if(head == null)
+        return null;
+    if(head.next == null)
+        return head;
+    ArrayList<Node> l = new ArrayList<Node>();
+    Node cur = head;
+    while(cur != null) {
+        l.add(cur);
+        cur = cur.next;
+    }
+    for(int i = l.size()-1; i > 0; i--) {
+        l.get(i).next = l.get(i-1);
+    }
+    l.get(0).next = null;
+    return l.get(l.size()-1);
+}
