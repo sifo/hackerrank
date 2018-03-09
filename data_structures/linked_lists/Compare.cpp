d0ac87ce4bf6856d2d9c60ce3ebee49fc66dd88b
Jean Vilver <jean.vilver@gmail.com>
2018-03-09 19:58:28 +0100

    Add Compare two linked lists

---
+++ data_structures/linked_lists/Compare.cpp
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/compare-two-linked-lists/problem
+/*
+  Compare two linked lists A and B
+  Return 1 if they are identical and 0 if they are not. 
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+int CompareLists(Node *headA, Node* headB)
+{
+    if(headA != NULL && headB != NULL && headA->data != headB->data)
+        return 0;
+    if((headA == NULL && headB != NULL) || (headA != NULL && headB == NULL))
+        return 0;
+    if(headA == NULL && headB == NULL)
+        return 1;
+    return CompareLists(headA->next, headB->next);
+}
