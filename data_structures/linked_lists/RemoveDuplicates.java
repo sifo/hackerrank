e3c128220cef1b553f3b7a42d0edb4dd7cf6a9ad
Jean Vilver <jean.vilver@gmail.com>
2018-03-10 21:17:35 +0100

    Refactor

--- data_structures/linked_lists/RemoveDuplicates.java
+++ data_structures/linked_lists/RemoveDuplicates.java
@@ -12,16 +12,17 @@ Node RemoveDuplicates(Node head) {
     Node cur = head;
     Node tmp = null;
     while(cur != null) {
-        if(tmp != null && cur.data != tmp.data) {
-            tmp.next = cur;
+        if(tmp != null && cur.next == null) {
+            tmp.next = null;
+            break;
+        }
+        if(tmp != null && cur.next.data != tmp.data) {
+            tmp.next = cur.next;
             tmp = null;
         }
         if(tmp == null && cur.next != null && cur.data == cur.next.data)
             tmp = cur;
         cur = cur.next;
-    }
-    if(tmp != null) {
-        tmp.next = null;
     }
     return head;
 }
