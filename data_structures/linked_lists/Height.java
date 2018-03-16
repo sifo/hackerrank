9a38cee624902fdc40c24c3a4cc3529b704ae942
Jean Vilver <jean.vilver@gmail.com>
2018-03-16 20:55:14 +0100

    Add Tree: Height of a Binary Tree

---
+++ data_structures/linked_lists/Height.java
@@ -0,0 +1,57 @@
+// https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+    static int height(Node root) {
+        int l = 0;
+        int r = 0;
+        if(root.left != null)
+            l = 1 + height(root.left);
+        if(root.right != null)
+            r = 1 + height(root.right);
+        return l > r ? l : r;
+    }
+    public static Node insert(Node root, int data) {
+        if(root == null){
+            return new Node(data);
+        }
+        else {
+            Node cur;
+            if(data <= root.data){
+                cur = insert(root.left, data);
+                root.left = cur;
+            }
+            else{
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0){
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }
+}
