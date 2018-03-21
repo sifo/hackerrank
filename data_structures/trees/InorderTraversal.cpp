9110ebaa31308666617b51eadc3b5d9a12bc4845
Jean Vilver <jean.vilver@gmail.com>
2018-03-21 23:30:59 +0100

    Remove wrong function

--- data_structures/trees/InorderTraversal.cpp
+++ data_structures/trees/InorderTraversal.cpp
@@ -19,30 +19,3 @@ void inOrder(node *root) {
     cout << root->data << " ";
     inOrder(root->right);
 }
-
-////////////////////////
-
-bool compareNode(node * x, node * y) {
-    return x->data < y->data;
-}
-
-void inOrder(node *root) {
-    if(root == NULL)
-        return;
-    vector<node*> values;
-    list<node*> queue;
-    queue.push_back(root);
-    while(!queue.empty()) {
-        node * f = queue.front();
-        queue.pop_front();
-        values.push_back(f);
-        if(f->right != NULL)
-            queue.push_back(f->right);
-        if(f->left != NULL)
-            queue.push_back(f->left);
-    }
-    std::sort(values.begin(), values.end(), compareNode);
-    for(auto i: values) {
-        cout << i->data << " ";
-    }
-}
