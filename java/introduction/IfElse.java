8233bf42ed7199a9a4f4e0ef1df2d65c34d20a4c
Jean Vilver <jean.vilver@gmail.com>
2018-03-25 21:53:50 +0200

    Add Java If-Else

---
+++ java/introduction/IfElse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class IfElse {
+
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n % 2 == 1) {
+            System.out.println("Weird");
+        } else if(n % 2 == 0) {
+            if(n >= 2 && n <= 5) {
+                System.out.println("Not Weird");
+            } else if(n >= 6 && n <= 20) {
+                System.out.println("Weird");
+            } else if(n > 20){
+                System.out.println("Not Weird");
+            }
+        }
+    }
+}
