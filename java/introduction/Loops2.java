6098e5376301137f3315c4a4d7caf97cd5f521e8
Jean Vilver <jean.vilver@gmail.com>
2018-03-28 20:55:41 +0200

    Add Java Loops II

---
+++ java/introduction/Loops2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Loop2 {
+
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            double res = a+b;
+            System.out.printf("%.0f ", res);
+            for(int j = 1; j < n; j++) {
+                res = res + Math.pow(2, j)*b;
+                System.out.printf("%.0f ", res);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
