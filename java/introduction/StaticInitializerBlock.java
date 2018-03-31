8ed86073fd1b11d56f4a3e5ef2dfa6d46ddeb83c
Jean Vilver <jean.vilver@gmail.com>
2018-03-31 22:02:41 +0200

    Add Java Static Initializer Block

---
+++ java/introduction/StaticInitializerBlock.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StaticInitializerBlock {
+
+    static Scanner sc = new Scanner(System.in);
+    static int H = sc.nextInt();
+    static int B = sc.nextInt();
+    static boolean flag = false;
+
+    static {
+        try {
+            if(B <= 0 || H <= 0) {
+                throw new Exception("Breadth and height must be positive");
+            }
+            flag = true;
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+    }
+
+}
