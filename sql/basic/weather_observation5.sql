0b607d73a21b074aa25e6fe8d91042b4b0e93ffe
Jean Vilver <jean.vilver@gmail.com>
2018-04-22 21:51:35 +0200

    Add Weather Observation Station 5

---
+++ sql/basic/weather_observation5.sql
@@ -0,0 +1,4 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+select city, length(city) from station order by length(city), city limit 1;
+select city, length(city) from station order by length(city) desc, city limit 1;
