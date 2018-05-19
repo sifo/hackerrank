490f6c51f52d03ca3d12a260752251e48f46d8e8
Jean Vilver <jean.vilver@gmail.com>
2018-05-19 23:58:56 +0200

    Add The PADS

---
+++ sql/advanced_select/the_pads.sql
@@ -0,0 +1,5 @@
+-- https://www.hackerrank.com/challenges/the-pads/problem
+
+select concat(name, '(', substring(occupation, 1, 1), ')') from occupations order by name;
+
+select concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.') from occupations group by occupation order by count(occupation);
