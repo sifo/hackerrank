6660fa7d07c00a487c028fe7792fd8df9dd17ff3
Jean Vilver <jean.vilver@gmail.com>
2018-06-06 19:37:14 +0200

    Add Java BitSet

---
+++ java/data_structures/JavaBitSet.java
@@ -0,0 +1,53 @@
+// https://www.hackerrank.com/challenges/java-bitset/problem
+
+import java.util.Scanner;
+import java.util.BitSet;
+
+class JavaBitSet {
+
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        BitSet b1 = new BitSet();
+        BitSet b2 = new BitSet();
+        for(int i = 0; i < m; i++) {
+            String cmd = sc.next();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            switch(cmd) {
+            case "AND":
+                if(x == 1)
+                    b1.and(b2);
+                else
+                    b2.and(b1);
+                break;
+            case "OR":
+                if(x == 1)
+                    b1.or(b2);
+                else
+                    b2.or(b1);
+                break;
+            case "XOR":
+                if(x == 1)
+                    b1.xor(b2);
+                else
+                    b2.xor(b1);
+                break;
+            case "FLIP":
+                if(x == 1)
+                    b1.flip(y);
+                else
+                    b2.flip(y);
+                break;
+            case "SET":
+                if(x == 1)
+                    b1.set(y);
+                else
+                    b2.set(y);
+                break;
+            }
+            System.out.println(b1.cardinality() + " " + b2.cardinality());
+        }
+    }
+}
