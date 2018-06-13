8dc1bd3d041bf03453c540a0755a2877be230525
Jean Vilver <jean.vilver@gmail.com>
2018-06-13 19:29:43 +0200

    Add Java Reflection - Attributes

---
+++ java/advanced/Reflection.java
@@ -0,0 +1,39 @@
+// https://www.hackerrank.com/challenges/java-reflection-attributes/problem
+
+import java.util.*;
+import java.lang.reflect.*;
+
+class Student{
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void anothermethod(){  }
+}
+
+public class Reflection {
+
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method m: methods){
+            methodList.add(m.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+}
