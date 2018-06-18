91f16eb95d00d675597c18081c61a353c694c50c
Jean Vilver <jean.vilver@gmail.com>
2018-06-18 23:37:37 +0200

    Add Basic Data Types

---
+++ c++/introduction/datatypes.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a; long b; char c; float d; double e;
+    cout.precision(10);
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl << b << endl << c << endl << d << endl << e << endl;
+}
