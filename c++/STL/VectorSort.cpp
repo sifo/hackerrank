d7b1621ced40f2121c02207897dff7cd2b793ff5
Jean Vilver <jean.vilver@gmail.com>
2018-08-13 23:31:41 +0200

    Add Vector-Sort

---
+++ c++/STL/VectorSort.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, e;
+    cin >> n;
+    vector<int> v;
+    while(n-- > 0) {
+        cin >> e;
+        v.push_back(e);
+    }
+    sort(v.begin(), v.end());
+    for(auto i: v) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
