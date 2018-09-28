bf44b5013ea6e039340877624d9ed5837b6e44e4
Jean Vilver <jean.vilver@gmail.com>
2018-09-28 23:00:32 +0200

    Add Procs

---
+++ ruby/methods/procs.rb
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/ruby-procs/problem
+
+def square_of_sum (my_array, proc_square, proc_sum)
+    sum = proc_sum.call(my_array)
+    proc_square.call(sum)
+end
+
+proc_square_number = proc { |n| n*n }
+proc_sum_array     = proc { |a| a.reduce(0, :+) }
+my_array = gets.split().map(&:to_i)
+
+puts square_of_sum(my_array, proc_square_number, proc_sum_array)
