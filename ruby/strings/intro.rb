97b3e0874c77723a6c8167ee1802fa60a88fd91a
Jean Vilver <jean.vilver@gmail.com>
2018-10-04 14:45:13 +0200

    Add Ruby - Strings - Introduction

---
+++ ruby/strings/intro.rb
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/ruby-strings-introduction/problem
+
+def single_quote
+  'Hello World and others!'
+end
+
+def double_quote
+  "Hello World and others!"
+end
+
+def here_doc
+  <<-QQ
+    Hello World and others!
+  QQ
+end
